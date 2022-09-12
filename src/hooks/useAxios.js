4626d5ff41fc270f992bc9711a4c604bcd72e3f1
Tina Flower <tmflower@gmail.com>
2022-09-12 12:26:09 -1000

    fixed issued with useAxios hook

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -4,10 +4,12 @@ import axios from "axios";
 const useAxios = (url) => {
     const [response, setResponse] = useState([]);
     
-    const addData = async () => {
-        // if extension is an empty string, PlayingCardList works fine
-        let extension = "";
-        // how can I assign the value of extension to another string, when one exists? 
+    const addData = async (extension) => {
+        // if there is no extension, replace with empty string so that only base url is used
+        if (!extension) {
+            extension = "";
+        }
+        // if there is an extension, include this at the end of the base url when making axios request
         const res = await axios.get(`${url}${extension}`);
         setResponse(data => [...data, res.data]);
     }
